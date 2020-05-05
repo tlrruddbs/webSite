cb3ad2870454dd67669721777257bb4b095d4294
tlrruddbs <tlrruddbs@DESKTOP-7TBVO6Q>
2020-05-05 15:07:54 +0900

    weatherVo

---
+++ src/main/java/org/hello/vo/WeatherVo.java
@@ -0,0 +1,30 @@
+package org.hello.vo;
+
+public class WeatherVo {
+	Double temp;
+	Object weather;
+	Object Name;
+	
+	public Double getTemp() {
+		return temp;
+	}
+	public void setTemp(Double temp) {
+		this.temp = temp;
+	}
+	public Object getWeather() {
+		return weather;
+	}
+	public void setWeather(Object weather) {
+		this.weather = weather;
+	}
+	public Object getName() {
+		return Name;
+	}
+	public void setName(Object name) {
+		Name = name;
+	}
+	
+	
+}
+	
+
